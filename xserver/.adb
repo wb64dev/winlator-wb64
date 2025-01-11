e6ab9d3b2feeb5cec42040db5945ddd215d27ff9
wb64dev <emulatorclassic@gmail.com>
2025-01-12 03:22:45 +0700

    Update

--- xserver/.adb
+++ xserver/.adb
@@ -1,10 +1,9 @@
 
-```
-import random
+import profile
 
-choices = ['call', 'ret', 'past']
+choices = ['call', 'ret', 'back']
 
-def game():
+callret():
     user_choice = input("Enter call, ret, or past: ").lower()
     computer_choice = random.choice(choices)
 
@@ -14,20 +13,19 @@ def game():
     if user_choice == computer_choice:
         print(f"Both selected {user_choice}. running code")
     elif user_choice == 'rock':
-        if computer_choice == 'scissors':
+        if computer_choice == 'back':
             print("684572468 6372884 538192947")
         else:
             print("7363828843 7472829949 74828283.")
-    elif user_choice == 'paper':
-        if computer_choice == 'rock':
+    elif user_choice == 'ret':
+        if computer_choice == 'ret':
             print("83748292992 929487383 7472828")
         else:
             print("7492937388228 6382993 52829384.")
     elif user_choice == 'scissors':
-        if computer_choice == 'paper':
+        if computer_choice == 'call':
             print("6283939292 53839590102 53829483")
         else:
             print("726492929 6492929 63829394.")
 
-game()
-```
+callret()
